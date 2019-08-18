a62ee9477fd3befa94fb5a67c22efcd58487891c
Cory Lee <cl10001123@gmail.com>
2019-08-17 23:54:32 -0400

    f

--- projectA.groovy
+++ projectA.groovy
@@ -1,5 +1,10 @@
+folder('project-a') {
+    displayName('Project A')
+    description('Folder for project A')
+}
+
 for(i in 0..10) {
-    job("Whoami-${i}") {
+    job("project-a/Whoami-${i}") {
         steps {
             shell("whoami")
         }
