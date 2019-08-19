2c2265b1e9582335c605b8017ae49005ec60b050
Cory Lee <cl10001123@gmail.com>
2019-08-18 20:22:47 -0400

    x

---
+++ _SecretsAndFolders.groovy
@@ -0,0 +1,18 @@
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import com.cloudbees.plugins.credentials.domains.*;
+
+String keyfile = "/tmp/key"
+
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "user", "password")
+
+
+def ksm1 = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(keyfile)
+Credentials ck1 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "password", ksm1)
+
+def ksm2 = new CertificateCredentialsImpl.UploadedKeyStoreSource(keyfile)
+Credentials ck2 = new CertificateCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "password", ksm2)
+
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck1)
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), ck2)
