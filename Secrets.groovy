184b7ba882c8aa8afd025f3b960d177fc3328635
Cory Lee <cl10001123@gmail.com>
2019-08-18 21:27:16 -0400

    x

--- Secrets.groovy
+++ Secrets.groovy
@@ -1,7 +1,3 @@
-import com.cloudbees.plugins.credentials.impl.*;
-import com.cloudbees.plugins.credentials.*;
-import com.cloudbees.plugins.credentials.domains.*;
-
 String keyfile = "/tmp/key"
 
 Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,java.util.UUID.randomUUID().toString(), "description", "user", "password")
